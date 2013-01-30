ac11bca39484e59450bffb5a1a1ea05018ecdce6
Francesco Chicchiriccò <ilgrosso@apache.org>
2013-01-30 14:50:54 +0100

    Making changes for working with Syncope 1.0.X

--- src/main/java/net/tirasa/syncoperestclient/App.java
+++ src/main/java/net/tirasa/syncoperestclient/App.java
@@ -3,41 +3,11 @@ package net.tirasa.syncoperestclient;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.UUID;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.syncope.client.http.PreemptiveAuthHttpRequestFactory;
-import org.apache.syncope.client.services.proxy.ConfigurationServiceProxy;
-import org.apache.syncope.client.services.proxy.ConnectorServiceProxy;
-import org.apache.syncope.client.services.proxy.EntitlementServiceProxy;
-import org.apache.syncope.client.services.proxy.LoggerServiceProxy;
-import org.apache.syncope.client.services.proxy.NotificationServiceProxy;
-import org.apache.syncope.client.services.proxy.PolicyServiceProxy;
-import org.apache.syncope.client.services.proxy.ReportServiceProxy;
-import org.apache.syncope.client.services.proxy.ResourceServiceProxy;
-import org.apache.syncope.client.services.proxy.RoleServiceProxy;
-import org.apache.syncope.client.services.proxy.SchemaServiceProxy;
-import org.apache.syncope.client.services.proxy.TaskServiceProxy;
-import org.apache.syncope.client.services.proxy.UserRequestServiceProxy;
-import org.apache.syncope.client.services.proxy.UserServiceProxy;
-import org.apache.syncope.client.services.proxy.WorkflowServiceProxy;
-import org.apache.syncope.common.mod.AttributeMod;
-import org.apache.syncope.common.services.ConfigurationService;
-import org.apache.syncope.common.services.ConnectorService;
-import org.apache.syncope.common.services.EntitlementService;
-import org.apache.syncope.common.services.LoggerService;
-import org.apache.syncope.common.services.NotificationService;
-import org.apache.syncope.common.services.ReportService;
-import org.apache.syncope.common.services.ResourceService;
-import org.apache.syncope.common.services.RoleService;
-import org.apache.syncope.common.services.SchemaService;
-import org.apache.syncope.common.services.TaskService;
-import org.apache.syncope.common.services.UserRequestService;
-import org.apache.syncope.common.services.UserService;
-import org.apache.syncope.common.services.WorkflowService;
-import org.apache.syncope.common.to.AttributeTO;
-import org.apache.syncope.common.to.RoleTO;
-import org.apache.syncope.common.to.UserTO;
+import org.apache.syncope.client.to.AttributeTO;
+import org.apache.syncope.client.to.UserTO;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.client.RestTemplate;
 
@@ -50,93 +20,58 @@ public class App {
 
     private static final String BASE_URL = (String) CTX.getBean("baseURL");
 
-    private static UserService userService;
-
-    private static RoleService roleService;
-
-    private static ResourceService resourceService;
-
-    private static EntitlementService entitlementService;
-
-    private static ConfigurationService configurationService;
-
-    private static ConnectorService connectorService;
-
-    private static LoggerService loggerService;
-
-    private static ReportService reportService;
-
-    private static TaskService taskService;
-
-    private static WorkflowService workflowService;
-
-    private static NotificationService notificationService;
-
-    private static SchemaService schemaService;
-
-    private static UserRequestService userRequestService;
-
-    private static PolicyServiceProxy policyService;
-
-    private static AttributeTO attributeTO(final String schema, final String value) {
-        AttributeTO attr = new AttributeTO();
-        attr.setSchema(schema);
-        attr.addValue(value);
-        return attr;
-    }
-
-    private static AttributeMod attributeMod(final String schema, final String valueToBeAdded) {
-        AttributeMod attr = new AttributeMod();
-        attr.setSchema(schema);
-        attr.addValueToBeAdded(valueToBeAdded);
-        return attr;
-    }
-
-    private static String getUUIDString() {
-        return UUID.randomUUID().toString().substring(0, 8);
-    }
-
-    private static RoleTO buildBasicRoleTO(final String name) {
-        RoleTO roleTO = new RoleTO();
-        roleTO.setName(name + getUUIDString());
-        roleTO.setParent(8L);
-        return roleTO;
-    }
-
-    private static RoleTO buildRoleTO(final String name) {
-        RoleTO roleTO = buildBasicRoleTO(name);
-
-        // verify inheritance password and account policies
-        roleTO.setInheritAccountPolicy(false);
-        // not inherited so setter execution shouldn't be ignored
-        roleTO.setAccountPolicy(6L);
-
-        roleTO.setInheritPasswordPolicy(true);
-        // inherited so setter execution should be ignored
-        roleTO.setPasswordPolicy(2L);
-
-        roleTO.addAttribute(attributeTO("icon", "anIcon"));
-
-        roleTO.addResource("resource-ldap");
-        return roleTO;
-    }
-
     public static UserTO getSampleTO(final String email) {
-        String uid = email;
         UserTO userTO = new UserTO();
         userTO.setPassword("password123");
-        userTO.setUsername(uid);
+        userTO.setUsername(email);
 
-        userTO.addAttribute(attributeTO("fullname", uid));
-        userTO.addAttribute(attributeTO("firstname", uid));
-        userTO.addAttribute(attributeTO("surname", "surname"));
-        userTO.addAttribute(attributeTO("type", "a type"));
-        userTO.addAttribute(attributeTO("userId", uid));
-        userTO.addAttribute(attributeTO("email", uid));
+        AttributeTO fullnameTO = new AttributeTO();
+        fullnameTO.setSchema("fullname");
+        fullnameTO.addValue(email);
+        userTO.addAttribute(fullnameTO);
+
+        AttributeTO firstnameTO = new AttributeTO();
+        firstnameTO.setSchema("firstname");
+        firstnameTO.addValue(email);
+        userTO.addAttribute(firstnameTO);
+
+        AttributeTO surnameTO = new AttributeTO();
+        surnameTO.setSchema("surname");
+        surnameTO.addValue("Surname");
+        userTO.addAttribute(surnameTO);
+
+        AttributeTO typeTO = new AttributeTO();
+        typeTO.setSchema("type");
+        typeTO.addValue("a type");
+        userTO.addAttribute(typeTO);
+
+        AttributeTO userIdTO = new AttributeTO();
+        userIdTO.setSchema("userId");
+        userIdTO.addValue(email);
+        userTO.addAttribute(userIdTO);
+
+        AttributeTO emailTO = new AttributeTO();
+        emailTO.setSchema("email");
+        emailTO.addValue(email);
+        userTO.addAttribute(emailTO);
+
+        AttributeTO loginDateTO = new AttributeTO();
+        loginDateTO.setSchema("loginDate");
         DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        userTO.addAttribute(attributeTO("loginDate", sdf.format(new Date())));
-        userTO.addDerivedAttribute(attributeTO("cn", null));
-        userTO.addVirtualAttribute(attributeTO("virtualdata", "virtualvalue"));
+        loginDateTO.addValue(sdf.format(new Date()));
+        userTO.addAttribute(loginDateTO);
+
+        // add a derived attribute
+        AttributeTO cnTO = new AttributeTO();
+        cnTO.setSchema("cn");
+        userTO.addDerivedAttribute(cnTO);
+
+        // add a virtual attribute
+        AttributeTO virtualdata = new AttributeTO();
+        virtualdata.setSchema("virtualdata");
+        virtualdata.addValue("virtualvalue");
+        userTO.addVirtualAttribute(virtualdata);
+
         return userTO;
     }
 
@@ -145,21 +80,6 @@ public class App {
                 ((PreemptiveAuthHttpRequestFactory) restTemplate.getRequestFactory());
         ((DefaultHttpClient) requestFactory.getHttpClient()).getCredentialsProvider().setCredentials(
                 requestFactory.getAuthScope(), new UsernamePasswordCredentials("admin", "password"));
-
-        userService = new UserServiceProxy(BASE_URL, restTemplate);
-        roleService = new RoleServiceProxy(BASE_URL, restTemplate);
-        resourceService = new ResourceServiceProxy(BASE_URL, restTemplate);
-        entitlementService = new EntitlementServiceProxy(BASE_URL, restTemplate);
-        configurationService = new ConfigurationServiceProxy(BASE_URL, restTemplate);
-        connectorService = new ConnectorServiceProxy(BASE_URL, restTemplate);
-        loggerService = new LoggerServiceProxy(BASE_URL, restTemplate);
-        reportService = new ReportServiceProxy(BASE_URL, restTemplate);
-        taskService = new TaskServiceProxy(BASE_URL, restTemplate);
-        policyService = new PolicyServiceProxy(BASE_URL, restTemplate);
-        workflowService = new WorkflowServiceProxy(BASE_URL, restTemplate);
-        notificationService = new NotificationServiceProxy(BASE_URL, restTemplate);
-        schemaService = new SchemaServiceProxy(BASE_URL, restTemplate);
-        userRequestService = new UserRequestServiceProxy(BASE_URL, restTemplate);
     }
 
     public static void main(final String[] args)
@@ -167,8 +87,5 @@ public class App {
 
         init();
 
-        System.out.println("AAAA\n"
-                + restTemplate.getForObject(BASE_URL + "user/request/create/allowed", Boolean.class));
-                //+ userRequestService.getOptions().getHeaderString(UserRequestService.SYNCOPE_CREATE_ALLOWED));
     }
 }
